3a72f4462b3e0d5b7b46fe33eef91381413693db
unknown <FlorianSchmitz1202@gmail.com>
2020-07-28 09:49:37 +0200

    Updates

---
+++ precache-manifest.0d075ac7270a0ee91c724e737a5408d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41955d04e3d7afff0d3192fbea33ffe5",
+    "url": "/copoc/index.html"
+  },
+  {
+    "revision": "56ea2f297fd8a61e8b85",
+    "url": "/copoc/static/css/main.5a3ec948.chunk.css"
+  },
+  {
+    "revision": "52311d5270d20a3b589c",
+    "url": "/copoc/static/js/2.7a671272.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/copoc/static/js/2.7a671272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56ea2f297fd8a61e8b85",
+    "url": "/copoc/static/js/main.0adac106.chunk.js"
+  },
+  {
+    "revision": "76c0a844589db63511d8",
+    "url": "/copoc/static/js/runtime-main.569b7175.js"
+  }
+]);
